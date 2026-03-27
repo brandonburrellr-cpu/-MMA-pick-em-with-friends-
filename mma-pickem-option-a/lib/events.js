287b40970136123bb8e6a673fcf26f8255a891dd
brandonburrellr-cpu <brandonburrellr@gmail.com>
2026-03-27 01:15:51 -0700

    Update events.js

--- mma-pickem-option-a/lib/events.js
+++ mma-pickem-option-a/lib/events.js
@@ -1,54 +1,54 @@
 export const EVENTS = [
   {
-    id: 'ufc-fn-seattle',
-    title: 'UFC Fight Night: Adesanya vs. Pyfer',
-    venue: 'Seattle, WA',
-    date: '2026-03-28T19:00:00-07:00',
+    id: 'ufc-fn-adesanya-pyfer',
+    name: 'UFC Fight Night: Adesanya vs. Pyfer',
+    location: 'Seattle, WA',
+    date: '2026-03-28T19:00:00',
     fights: [
-      ['Israel Adesanya', 'Joe Pyfer'],
-      ['Alexa Grasso', 'Maycee Barber'],
-      ['Bryce Mitchell', 'Dan Ige'],
-      ['Michel Pereira', 'Roman Kopylov'],
-      ['Nassourdine Imavov', 'Paulo Costa']
-    ]
+      { id: 'fight_1', red: 'Israel Adesanya', blue: 'Joe Pyfer' },
+      { id: 'fight_2', red: 'Alexa Grasso', blue: 'Maycee Barber' },
+      { id: 'fight_3', red: 'Bryce Mitchell', blue: 'Dan Ige' },
+      { id: 'fight_4', red: 'Michel Pereira', blue: 'Roman Kopylov' },
+      { id: 'fight_5', red: 'Nassourdine Imavov', blue: 'Paulo Costa' },
+    ],
   },
   {
-    id: 'ufc-327',
-    title: 'UFC 327: Procházka vs. Ulberg',
-    venue: 'Las Vegas, NV',
-    date: '2026-04-11T19:00:00-07:00',
+    id: 'ufc-327-prochazka-ulberg',
+    name: 'UFC 327: Procházka vs. Ulberg',
+    location: 'Las Vegas, NV',
+    date: '2026-04-11T19:00:00',
     fights: [
-      ['Jiří Procházka', 'Carlos Ulberg'],
-      ['Tatsuro Taira', 'Joshua Van'],
-      ['Mateusz Gamrot', 'Benoît Saint Denis'],
-      ['Amanda Lemos', 'Virna Jandiroba'],
-      ['Dominick Reyes', 'Aleksandar Rakić']
-    ]
+      { id: 'fight_1', red: 'Jiří Procházka', blue: 'Carlos Ulberg' },
+      { id: 'fight_2', red: 'Tatsuro Taira', blue: 'Joshua Van' },
+      { id: 'fight_3', red: 'Mateusz Gamrot', blue: 'Benoît Saint Denis' },
+      { id: 'fight_4', red: 'Amanda Lemos', blue: 'Mackenzie Dern' },
+      { id: 'fight_5', red: 'Geoff Neal', blue: 'Ian Machado Garry' },
+    ],
   },
   {
-    id: 'ufc-fn-winnipeg',
-    title: 'UFC Fight Night: Burns vs. Malott',
-    venue: 'Winnipeg, Canada',
-    date: '2026-04-18T18:00:00-07:00',
+    id: 'ufc-fn-burns-malott',
+    name: 'UFC Fight Night: Burns vs. Malott',
+    location: 'Winnipeg, Canada',
+    date: '2026-04-18T18:00:00',
     fights: [
-      ['Gilbert Burns', 'Mike Malott'],
-      ['Charles Jourdain', 'Jack Shore'],
-      ['Jasmine Jasudavicius', 'Erin Blanchfield'],
-      ['Brad Katona', 'Raoni Barcelos'],
-      ['Gavin Tucker', 'Dooho Choi']
-    ]
+      { id: 'fight_1', red: 'Gilbert Burns', blue: 'Mike Malott' },
+      { id: 'fight_2', red: 'Charles Jourdain', blue: 'Arnold Allen' },
+      { id: 'fight_3', red: 'Jasmine Jasudavicius', blue: 'Erin Blanchfield' },
+      { id: 'fight_4', red: 'Brad Katona', blue: 'Raoni Barcelos' },
+      { id: 'fight_5', red: 'Marc-Andre Barriault', blue: 'Roman Dolidze' },
+    ],
   },
   {
-    id: 'ufc-fn-macau',
-    title: 'UFC Fight Night: Song vs. Figueiredo',
-    venue: 'Macau, China',
-    date: '2026-05-30T18:00:00-07:00',
+    id: 'ufc-fn-song-figueiredo',
+    name: 'UFC Fight Night: Song vs. Figueiredo',
+    location: 'Macau, China',
+    date: '2026-05-30T18:00:00',
     fights: [
-      ['Song Yadong', 'Deiveson Figueiredo'],
-      ['Anthony Hernandez', 'Roman Dolidze'],
-      ['Serghei Spivac', 'Jailton Almeida'],
-      ['Alex Perez', 'Su Mudaerji'],
-      ['Zhang Mingyang', 'Alonzo Menifield']
-    ]
-  }
+      { id: 'fight_1', red: 'Song Yadong', blue: 'Deiveson Figueiredo' },
+      { id: 'fight_2', red: 'Zhang Mingyang', blue: 'Alonzo Menifield' },
+      { id: 'fight_3', red: 'Sergei Pavlovich', blue: 'Jailton Almeida' },
+      { id: 'fight_4', red: 'Alex Perez', blue: 'Su Mudaerji' },
+      { id: 'fight_5', red: 'Yan Xiaonan', blue: 'Amanda Ribas' },
+    ],
+  },
 ];
